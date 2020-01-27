51f88f38083d42933f922cdb521d19bcc30fbe90
tksagosago <senkanyamayama@gmail.com>
2020-01-27 15:58:05 +0900

    fix_index

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,88 +1,106 @@
 class AccountController < ApplicationController
+  before_action :set_date, only: [:new, :index]
 
   def index
+    @monthly = Expenditure.group("DATE_FORMAT(created_at, '%Y%m')")
   end
 
   def new
     @incomes      = Income.new
     @expenditures = Expenditure.new
-    now = Time.current
     @exp_graph_data = []
 
-    @billing = Expenditure.where(expsubject: "課金", created_at: now.all_month)
+    @billing = Expenditure.where(expsubject: "課金", created_at: @now.all_month)
     @billing_sum = 0
     @billing.each do |billing|
       @billing_sum += billing.expamount
-      @exp_graph_data << @billing_sum
     end
+    @billing_sum__data = ['課金', @billing_sum]
+    @exp_graph_data << @billing_sum__data
 
-    @food = Expenditure.where(expsubject: "食費", created_at: now.all_month)
+    @food = Expenditure.where(expsubject: "食費", created_at: @now.all_month)
     @food_sum = 0
     @food.each do |food|
       @food_sum += food.expamount
     end
+    @food_sum__data = ['食費', @food_sum]
+    @exp_graph_data << @food_sum__data
 
-    @daily_necces = Expenditure.where(expsubject: "日用品費", created_at: now.all_month)
+    @daily_necces = Expenditure.where(expsubject: "日用品費", created_at: @now.all_month)
     @daily_necces_sum = 0
     @daily_necces.each do |daily_necces|
       @daily_necces_sum += daily_necces.expamount
     end
+    @daily_necces_sum__data = ['日用品費', @daily_necces_sum]
+    @exp_graph_data << @daily_necces_sum__data
 
-    @utility = Expenditure.where(expsubject: "水道光熱費", created_at: now.all_month)
+    @utility = Expenditure.where(expsubject: "水道光熱費", created_at: @now.all_month)
     @utility_sum = 0
     @utility.each do |utility|
       @utility_sum += utility.expamount
     end
+    @utility_sum__data = ['水道光熱費', @utility_sum]
+    @exp_graph_data << @utility_sum__data
 
-    @entertainment = Expenditure.where(expsubject: "娯楽費", created_at: now.all_month)
+    @entertainment = Expenditure.where(expsubject: "娯楽費", created_at: @now.all_month)
     @entertainment_sum = 0
     @entertainment.each do |entertainment|
       @entertainment_sum += entertainment.expamount
     end
+    @entertainment_sum__data = ['娯楽費', @entertainment_sum]
+    @exp_graph_data << @entertainment_sum__data
 
-    @rent = Expenditure.where(expsubject: "家賃", created_at: now.all_month)
+    @rent = Expenditure.where(expsubject: "家賃", created_at: @now.all_month)
     @rent_sum = 0
     @rent.each do |rent|
       @rent_sum += rent.expamount
     end
+    @rent_sum__data = ['家賃', @rent_sum]
+    @exp_graph_data << @rent_sum__data
 
-    @mobile = Expenditure.where(expsubject: "通信費", created_at: now.all_month)
+    @mobile = Expenditure.where(expsubject: "通信費", created_at: @now.all_month)
     @mobile_sum = 0
     @mobile.each do |mobile|
       @mobile_sum += mobile.expamount
     end
+    @mobile_sum__data = ['通信費', @mobile_sum]
+    @exp_graph_data << @mobile_sum__data
 
-    @scholarship = Expenditure.where(expsubject: "奨学金", created_at: now.all_month)
+    @scholarship = Expenditure.where(expsubject: "奨学金", created_at: @now.all_month)
     @scholarship_sum = 0
     @scholarship.each do |scholarship|
       @scholarship_sum += scholarship.expamount
     end
+    @scholarship_sum__data = ['奨学金', @scholarship_sum]
+    @exp_graph_data << @scholarship_sum__data
 
-    @others = Expenditure.where(expsubject: "その他", created_at: now.all_month)
+    @others = Expenditure.where(expsubject: "その他", created_at: @now.all_month)
     @others_sum = 0
     @others.each do |others|
       @others_sum += others.expamount
     end
+    @others_sum__data = ['その他', @others_sum]
+    @exp_graph_data << @others_sum__data
 
-    @salary = Income.where(incsubject: "給与", created_at: now.all_month)
+    @salary = Income.where(incsubject: "給与", created_at: @now.all_month)
     @salary_sum = 0
     @salary.each do |salary|
       @salary_sum += salary.incamount
     end
 
-    @inc_others = Income.where(incsubject: "雑益", created_at: now.all_month)
+    @inc_others = Income.where(incsubject: "雑益", created_at: @now.all_month)
     @inc_others_sum = 0
     @inc_others.each do |inc_others|
       @inc_others_sum += inc_others.incamount
     end
 
-    @exp_all = Expenditure.where(created_at: now.all_month)
+    @exp_all = Expenditure.where(created_at: @now.all_month)
     @exp_all_sum = 0
     @exp_all.each do |exp_all|
       @exp_all_sum += exp_all.expamount
     end
 
-    @inc_all = Income.where(created_at: now.all_month)
+    @inc_all = Income.where(created_at: @now.all_month)
     @inc_all_sum = 0
     @inc_all.each do |inc_all|
       @inc_all_sum += inc_all.incamount
@@ -111,6 +129,9 @@ class AccountController < ApplicationController
     end
   end
 
+  def show
+  end
+
 
   private
   def income_params
@@ -119,6 +140,10 @@ class AccountController < ApplicationController
 
   def expenditure_params
     params.permit(:expsubject, :expamount)
+  end
+
+  def set_date
+    @now = Time.current
   end
 
 end
