586efde8730d4fb87f6a95ce3af385fa5902cc02
tksagosago <senkanyamayama@gmail.com>
2020-01-30 17:01:07 +0900

    fix

--- app/assets/javascripts/add-form.js
+++ app/assets/javascripts/add-form.js
@@ -3,6 +3,8 @@ $(function(){
     var subject_value = document.getElementById('ex_sub').value;
     if (subject_value == 'その他' || subject_value == '課金'){
       $(".hidden").removeClass("hidden");
+    } else{
+      $(".app_new__contents__right__bottom__contents__spending__form__other__form").addClass("hidden");
     }
   });
  });
